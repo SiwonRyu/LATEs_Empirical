88d205a23a1ba12872b923381874175cba3a3593
Siwon <siwonryu91@gmail.com>
2024-05-04 13:46:01 -0500

    v240504

--- Empirical/mfiles/AtimesB_C.m
+++
@@ -1,7 +0,0 @@
-function AB = AtimesB_C(A,B)
-% A, B: NxKxG
-% A(:,:,g)'*B(:,:,g) = (KxN) x (NxK)
-
-    AB_tmp = permute(A,[2,1,4,3]).*permute(B,[4,1,2,3]);
-    AB = permute(sum(AB_tmp,2),[1,3,4,2]);    
-end
--- Empirical/mfiles/Inf_t.m
+++
@@ -1,7 +0,0 @@
-function [SE,t,p,Avar] = Inf_t(coef, resid, IF)
-    n = size(resid,1);
-    Avar = IF'*IF/n;
-    SE = sqrt(diag(Avar/n));
-    t = coef./SE;
-    p = 2*(1-cdf('T',abs(t),n)); % need to adjust DF
-end
